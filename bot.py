fffb63b9591f7d3cfaf6dd57a2608f451be22c41
GuoyJeng <guojing2549@gmail.com>
2024-11-07 14:35:45 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv
 import discord
+
+load_dotenv('heheha.env')
+token = os.getenv('DISCORD_TOKEN')
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -11,4 +16,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('my token goes here')
+client.run(token)
+
+print(f"Token loaded: {token}")  # Verify if the token is loaded correctly
