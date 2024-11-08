174d022a66fb945dc618cf7dff63f21209b713c8
GuoyJeng <guojing2549@gmail.com>
2024-11-08 14:14:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,12 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello World!')
+    
 intents = discord.Intents.default()
 intents.message_content = True
 
